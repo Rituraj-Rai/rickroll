2c7ea8a1d994655d4b6ce23cb0adb9fd3fdc28e3
Rituraj Ranjan Rai <65041351+Rituraj-Rai@users.noreply.github.com>
2021-04-13 10:43:00 +0530

    Add files via upload

---
+++ colorJoke.js
@@ -0,0 +1,17 @@
+document.querySelector("button").addEventListener("click", function () {
+  rickroll();
+  const count = document.createElement("div");
+  count.innerHTML = "<h2>CountDown..</h2>";
+  const num = document.createElement("h2");
+  count.append(num);
+  let i = 30;
+  const id = setInterval(() => {
+    i--;
+    num.innerText = i.toString();
+    if (i < 0) {
+      num.innerText = "Your Internet seems to be slow..Wait...";
+      clearInterval(id);
+    }
+  }, 1000);
+  document.body.append(count);
+});
